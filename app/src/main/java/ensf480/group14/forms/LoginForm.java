c153a0f98c4d40a0e849273ad7214b1a3ce28396
nickknapton12 <nickknapton12@gmail.com>
2021-12-04 15:22:39 -0700

    Login Form

--- app/src/main/java/ensf480/group14/forms/LoginForm.java
+++ app/src/main/java/ensf480/group14/forms/LoginForm.java
@@ -1,3 +1,116 @@
 package ensf480.group14.forms;
-public class LoginForm {
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class LoginForm implements Form{
+    private static LoginForm onlyInstance;
+    private String username;
+    private String password;
+
+    private LoginForm(){
+        username = new String();
+        password = new String();
+    }
+
+    public static LoginForm getOnlyInstance(){
+        if(onlyInstance == null){
+            onlyInstance = new LoginForm();
+        }
+        return onlyInstance;
+    }
+
+    public JPanel display(){
+        JPanel panel = new JPanel();
+        Dimension expectDimension = new Dimension(300, 300);
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0,1));
+
+        panel.setPreferredSize(expectDimension);
+        panel.setMaximumSize(expectDimension);
+        panel.setMinimumSize(expectDimension);
+
+        panel.setBackground(Color.GRAY);
+        
+        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel loginLabel = new JLabel("Login");
+        loginLabel.setFont(new Font("Serif", Font.BOLD, 35));
+        loginLabel.setSize(40,40);
+        panel.add(loginLabel);
+
+        panel.add(Box.createRigidArea(new Dimension(1, 5)));
+
+        panel.add(new JLabel("Username"));
+        JTextField usernameField = new JTextField();
+        usernameField.setSize(190, 20);
+        usernameField.setMaximumSize(new Dimension(190, 20));
+        panel.add(usernameField);
+
+        panel.add(new JLabel("Password"));
+        JTextField passwordField = new JTextField();
+        passwordField.setMaximumSize(new Dimension(190, 20));
+        panel.add(passwordField);
+
+        panel.add(Box.createRigidArea(new Dimension(1, 5)));
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt){
+                if(evt.getSource() == submitButton){
+                    username = usernameField.getText();
+                    password = passwordField.getText();
+                    login();
+                }
+            }
+        });
+        panel.add(submitButton);
+
+        return panel;
+    }
+
+    public void login(){
+        // This should query the db to verify password, ideally in the db controller?
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // For testing
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        LoginForm login = getOnlyInstance();
+        frame.add(login.display());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
