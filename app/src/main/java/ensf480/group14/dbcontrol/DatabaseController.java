519a69336284106a29f1ad2e2b3589c304ac844c
brooke-k <brooke.kindleman@gmail.com>
2021-12-04 17:25:33 -0700

    feat: Preliminary prog. on DBController and Report
    fix: digital duct tape is holding this together

--- app/src/main/java/ensf480/group14/dbcontrol/DatabaseController.java
+++ app/src/main/java/ensf480/group14/dbcontrol/DatabaseController.java
@@ -1,4 +1,200 @@
 package ensf480.group14.dbcontrol;
 
-public class DatabaseController {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import ensf480.group14.external.Property;
+import ensf480.group14.forms.Search;
+import ensf480.group14.users.Landlord;
+import ensf480.group14.users.Manager;
+import ensf480.group14.users.RegisteredRenter;
+
+public class DatabaseController implements DatabaseSubject {
+    public static boolean databaseOpen; // Public to allow for any
+    // calling function or class to mark it as "closed" if an exception
+    // regarding the database is thrown.
+
+    private ArrayList<RegisteredRenter> registeredRenters;
+    private ArrayList<Manager> managers;
+    private ArrayList<Landlord> landlords;
+
+    private static MongoClient mongoClient;
+    private static MongoDatabase dbMongo;
+    private static MongoCollection usersCollection;
+    private static MongoCollection propertiesCollection;
+    private static MongoCollection emailCollection;
+
+    public DatabaseController() {
+        mongoClient = null;
+        createConnection();
+    }
+
+    private void createConnection() {
+        if (mongoClient == null) {
+            File connectionFile = new File("./connection.txt");
+            if (!connectionFile.exists()) {
+                System.err.println("Connection information file \"./connection\" could not be located.");
+                System.err.println("Database is not accessible at this time.");
+                return;
+            }
+            String[] mongoStr = new String[3];
+            String rString = "";
+            String dbName = "";
+            try {
+                BufferedReader connectionReader = new BufferedReader(new FileReader(connectionFile));
+
+                rString = connectionReader.readLine();
+                int i = 0;
+                while (i < 3) {
+                    mongoStr[i] = rString;
+                    rString = connectionReader.readLine();
+                    i++;
+                }
+                if (rString != null) {
+                    dbName = rString;
+                }
+                connectionReader.close();
+            } catch (IOException e) {
+                System.err.println("File \"./connection\" could not be read: In DatabaseController.createConnection()");
+                e.printStackTrace();
+                return;
+            }
+
+            String connectionURL = "";
+            for (String m : mongoStr) {
+                connectionURL = connectionURL + m;
+            }
+
+            mongoClient = MongoClients.create(connectionURL);
+            dbMongo = mongoClient.getDatabase(dbName);
+
+            usersCollection = dbMongo.getCollection("Users");
+            propertiesCollection = dbMongo.getCollection("Properties");
+            emailCollection = dbMongo.getCollection("email");
+        }
+    }
+
+    @Override
+    public void addObserver(DatabaseObserver dbo) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void removeObserver(DatabaseObserver dbo) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void notifiyAllObservers() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void printUsers() {
+        FindIterable<Document> docIterator = usersCollection.find();
+        Iterator<Document> collectionIter = docIterator.iterator();
+        while (collectionIter.hasNext()) {
+            System.out.println(collectionIter.next());
+        }
+
+    }
+
+    private void printEmail() {
+        FindIterable<Document> docIterator = emailCollection.find();
+        Iterator<Document> collectionIter = docIterator.iterator();
+        while (collectionIter.hasNext()) {
+            System.out.println(collectionIter.next());
+        }
+
+    }
+
+    private void printProperties() {
+        FindIterable<Document> docIterator = propertiesCollection.find();
+        Iterator<Document> collectionIter = docIterator.iterator();
+        while (collectionIter.hasNext()) {
+            System.out.println(collectionIter.next());
+        }
+
+    }
+
+    public ArrayList<Property> getMatchingProperties(Search searchInfo) {
+        return null;
+    }
+
+    public ArrayList<Property> getPropertiesRentedIn(Calendar startingDate, Calendar endingDate) {
+        return null;
+    }
+
+    public Property getPropertyWithLandlord(int landlordID) {
+        return null;
+    }
+
+    public Property getPropertyByID(int iDnum) {
+        return null;
+    }
+
+           
+
+    }
+
+    public static void setDatabaseOpen(boolean databaseOpen) {
+        DatabaseController.databaseOpen = databaseOpen;
+    }
+
+    public ArrayList<RegisteredRenter> getRegisteredRenters() {
+        return registeredRenters;
+    }
+
+    public void setRegisteredRenters(ArrayList<RegisteredRenter> registeredRenters) {
+        this.registeredRenters = registeredRenters;
+    }
+
+    p
+
+        }
+
+    
+
+    
+    public void setManagers(ArrayList<Manager> managers) {
+        this.managers = managers;
+    }
+
+    public ArrayList<Landlord> getLandlords() {
+        return landlords;
+    }
+
+    public void setLandlords(ArrayList<Landlord> landlords) {
+        this.landlords = landlords;
+    }
+
+    public ArrayList<String> getAllProperties() {
+        return null;
+    }
+
+    public ArrayList<String> getAllUsers() {
+        return null;
+    }
+
+    public ArrayList<String> getAllEmails() {
+        return null;
+    }
+
 }
--- app/src/main/java/ensf480/group14/dbcontrol/Report.java
+++ app/src/main/java/ensf480/group14/dbcontrol/Report.java
@@ -1,7 +1,17 @@
 package ensf480.group14.dbcontrol;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Calendar;
+import java.util.Iterator;
 
+import javax.swing.text.Document;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
 
 public class Report {
     // Attributes
@@ -11,19 +21,47 @@ public class Report {
     private int numPropertiesRented;
     private int numPropertiesActive;
 
+    File reportFile; // Can be changed in the future,
+                     // used for testing purposes here\
+                     // Output file for the report to go to.
+    BufferedWriter bufReportWriter;
+
     // private ArrayList<Property> propertiesRented;
     // Currently throws an error because the Property class has not been made yet.
 
     // Methods
-    public Report(Calendar startDate, Calendar endDate){
+    public Report(Calendar startDate, Calendar endDate, DatabaseController controller) throws IOException {
         dateRangeStart = startDate;
         dateRangeEnd = endDate;
+        generateReport(controller);
     }
 
-    public String generateReport() {
+    public String generateReport(DatabaseController dbControl) throws IOException {
+
+        /*
+         * reportFile = new File("./src/main/outputs/report.txt");
+         * if (reportFile.exists()) {
+         * reportFile.delete(); // Done to clear any older report copies,
+         * // for testing
+         * reportFile.createNewFile();
+         * } else {
+         * reportFile.createNewFile();
+         * }
+         * 
+         * bufReportWriter = new BufferedWriter(new FileWriter(reportFile));
+         * 
+         * String reportString; // String-version of report to write
+         * MongoCollection propertyCollection = dbControl.getAllProperties();
+         * 
+         * FindIterable<Document> docIterator = propertyCollection.find();
+         * Iterator collectionIter = docIterator.iterator();
+         * while (collectionIter.hasNext()) {
+         * bufReportWriter.write(collectionIter.next().toString());
+         * }
+         * bufReportWriter.close();
+         */
         return null;
     }
-
 
     public Calendar getDateRangeStart() {
         return dateRangeStart;
