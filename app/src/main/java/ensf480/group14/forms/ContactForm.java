1de7491d99e3dfa8bbcd026d366bd2b094f7bed2
BrianKrameruc <73033196+BrianKrameruc@users.noreply.github.com>
2021-12-05 15:17:31 -0700

    Contact and Property Application
    will need to update layout for ui,
    contact needs to get landlords email(this could be done in email)

--- app/src/main/java/ensf480/group14/forms/ContactForm.java
+++ app/src/main/java/ensf480/group14/forms/ContactForm.java
@@ -1,3 +1,114 @@
 package ensf480.group14.forms;
-public class ContactForm {
+
+import ensf480.group14.external.Email;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+//import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ContactForm implements Form {
+    // private String userEmailAddress;
+    // private String message;
+
+    public JPanel display(){
+        JPanel panel = new JPanel();
+        Dimension expectDimension = new Dimension(300, 300);
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0,1));
+
+        panel.setPreferredSize(expectDimension);
+        panel.setMaximumSize(expectDimension);
+        panel.setMinimumSize(expectDimension);
+
+        panel.setBackground(Color.GRAY);
+        
+        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel contactLabel = new JLabel("Contact");
+        contactLabel.setFont(new Font("Serif", Font.BOLD, 35));
+        contactLabel.setSize(40,40);
+        panel.add(contactLabel);
+
+        panel.add(Box.createRigidArea(new Dimension(1, 5)));
+
+        panel.add(new JLabel("Enter Your Email Address"));
+        JTextField userEmail = new JTextField();
+        userEmail.setSize(190, 20);
+        userEmail.setMaximumSize(new Dimension(190, 20));
+        panel.add(userEmail);
+
+        panel.add(new JLabel("Body"));
+        JTextArea emailBody = new JTextArea(50,50);
+        //JScrollPane sPane = new JScrollPane(emailBody);
+        emailBody.setSize(300, 60);
+        emailBody.setMaximumSize(new Dimension(300, 60));
+        emailBody.setColumns(300);
+        emailBody.setLineWrap(true);
+        panel.add(emailBody);
+
+
+        panel.add(Box.createRigidArea(new Dimension(1, 5)));
+
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt){
+                if(evt.getSource() == sendButton){
+                    parseEmail(emailBody.getText(),userEmail.getText());
+                    
+                }
+            }
+        });
+        panel.add(sendButton);
+
+        return panel;
+    }
+
+
+    Email parseEmail(String body,String sendAddr){
+        Email mail = new Email(body,sendAddr);
+        return mail;
+    }
+
+   
+
+    
+
+   
+
+   
+
+   
+
+   
+
+   
+
+
+
+    // For testing
+    // public static void main(String[] args) {
+    //     JFrame frame = new JFrame();
+    //     ContactForm form = new ContactForm();
+    //     frame.add(form.display());
+    //     frame.setPreferredSize(new Dimension(524,368));
+        
+    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     frame.pack();
+    //     frame.setLocationRelativeTo(null);
+    //     frame.setVisible(true);
+    // }
 }
+
---
+++ app/src/main/java/ensf480/group14/forms/PropertyApplication.java
@@ -0,0 +1,211 @@
+package ensf480.group14.forms;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PropertyApplication implements Form {
+    // private String userEmailAddress;
+    // private String message;
+    int id;
+    String addr;
+    int numBed;
+    double numBath;
+    boolean furnish;
+    String cityQuad;
+    double price;
+
+    public JPanel display(){
+        JPanel panel = new JPanel();
+        Dimension expectDimension = new Dimension(300, 300);
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0,1));
+
+        panel.setPreferredSize(expectDimension);
+        panel.setMaximumSize(expectDimension);
+        panel.setMinimumSize(expectDimension);
+
+        panel.setBackground(Color.GRAY);
+        
+        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel contactLabel = new JLabel("Property Application");
+        contactLabel.setFont(new Font("Serif", Font.BOLD, 35));
+        contactLabel.setSize(40,40);
+        panel.add(contactLabel);
+
+        panel.add(Box.createRigidArea(new Dimension(1, 5)));
+
+        panel.add(new JLabel("Address"));
+        JTextField address = new JTextField();
+        address.setSize(190, 20);
+        address.setMaximumSize(new Dimension(190, 20));
+        panel.add(address);
+
+        panel.add(new JLabel("Number of Bedrooms"));
+        JTextField numBedrooms = new JTextField();
+        numBedrooms.setSize(190, 20);
+        numBedrooms.setMaximumSize(new Dimension(190, 20));
+        panel.add(numBedrooms);
+
+        panel.add(new JLabel("Number of Bathrooms"));
+        JTextField numBathrooms = new JTextField();
+        numBathrooms.setSize(190, 20);
+        numBathrooms.setMaximumSize(new Dimension(190, 20));
+        panel.add(numBathrooms);
+        
+        JLabel checkLabel = new JLabel("Furnished");
+        panel.add(checkLabel);
+        JCheckBox furnished = new JCheckBox();
+        furnished.setOpaque(false);
+
+        //furnished.setText("Furnished");
+        //furnished.setSize(20, 20);
+        furnished.setMaximumSize(new Dimension(190, 20));
+        panel.add(furnished);
+
+        furnished.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                JCheckBox cb = (JCheckBox) event.getSource();
+                if (cb.isSelected()) {
+                    furnish = true;
+                } else {
+                    furnish = false;
+                }
+            }
+        });
+
+
+        panel.add(new JLabel("City Quadrant"));
+        JTextField quad = new JTextField();
+        quad.setSize(190, 20);
+        quad.setMaximumSize(new Dimension(190, 20));
+        panel.add(quad);
+
+        panel.add(new JLabel("Price"));
+        JTextField price = new JTextField();
+        price.setSize(190, 20);
+        price.setMaximumSize(new Dimension(190, 20));
+        panel.add(price);
+
+
+        panel.add(Box.createRigidArea(new Dimension(1, 5)));
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt){
+                if(evt.getSource() == submitButton){
+                    setAddress(address.getText());
+                    setNumBed(Integer.parseInt(numBedrooms.getText()));
+                    setNumBath(Integer.parseInt(numBathrooms.getText()));
+                    setCityQuad(quad.getText());
+                    setPrice(Double.parseDouble(price.getText()));
+                    //signUp();
+                }
+            }
+        });
+        panel.add(submitButton);
+
+        return panel;
+    }
+
+
+    
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return addr;
+    }
+
+
+    public void setAddress(String address) {
+        this.addr = address;
+    }
+
+    public int getNumBed() {
+        return numBed;
+    }
+
+
+    public void setNumBed(int numBed) {
+        this.numBed = numBed;
+    }
+
+    public double getNumBath() {
+        return numBath;
+    }
+
+    public void setNumBath(double numBath) {
+        this.numBath = numBath;
+    }
+
+
+    public boolean isFurnished() {
+        return furnish;
+    }
+
+
+    public void setFurnish(boolean furnish) {
+        this.furnish = furnish;
+    }
+
+    public String getCityQuad() {
+        return cityQuad;
+    }
+
+
+    public void setCityQuad(String cityQuad) {
+        this.cityQuad = cityQuad;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
+
+
+
+
+    // For testing
+    // public static void main(String[] args) {
+    //     JFrame frame = new JFrame();
+    //     PropertyApplication form = new PropertyApplication();
+    //     frame.add(form.display());
+    //     frame.setPreferredSize(new Dimension(624,768));
+        
+    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     frame.pack();
+    //     frame.setLocationRelativeTo(null);
+    //     frame.setVisible(true);
+    // }
+}
+
