00d13354f05f99db6a36857af21e4a61511af244
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2021-12-05 12:28:24 -0700

    Finished the BillingController and BillingSystem classes

---
+++ app/src/main/java/ensf480/group14/billcontrol/BillingSystem.java
@@ -0,0 +1,44 @@
+package ensf480.group14.billcontrol;
+
+import java.util.Date;
+
+public class BillingSystem {
+    private double feeAmount;
+    private Date paymentDate;
+    private String paymentType;
+    private Landlord landlord;
+
+    //Constructor 
+    public BillingSystem(double feeAmount, Landlord landlord, Date paymentDate, String paymentType)
+	{
+		this.feeAmount = feeAmount;
+		this.landlord = landlord;
+		this.paymentDate = paymentDate;
+        this.paymentType = paymentType;
+	}
+    //Start of getters and setters 
+    public double getFeeAmount() {
+        return this.feeAmount;
+    }
+
+    public void setFeeAmount(double feeAmount) {
+        this.feeAmount = feeAmount;
+    }
+
+    public Date getPaymentDate() {
+        return this.paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPaymentType() {
+        return this.paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+}
