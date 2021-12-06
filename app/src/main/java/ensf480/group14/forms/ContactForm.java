cc5cc8f0b34aa45d17a8c44d2c225869e3862c99
BrianKrameruc <73033196+BrianKrameruc@users.noreply.github.com>
2021-12-06 09:40:21 -0700

    Update ContactForm.java

--- app/src/main/java/ensf480/group14/forms/ContactForm.java
+++ app/src/main/java/ensf480/group14/forms/ContactForm.java
@@ -6,10 +6,12 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -27,30 +29,34 @@ public class ContactForm implements Form {
         Dimension expectDimension = new Dimension(300, 300);
 
         panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-        panel.setLayout(new GridLayout(0,1));
-
+       // panel.setLayout(new GridLayout(0,1));
+        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
         panel.setPreferredSize(expectDimension);
         panel.setMaximumSize(expectDimension);
         panel.setMinimumSize(expectDimension);
 
         panel.setBackground(Color.GRAY);
         
-        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //panel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JLabel contactLabel = new JLabel("Contact");
         contactLabel.setFont(new Font("Serif", Font.BOLD, 35));
         contactLabel.setSize(40,40);
+        contactLabel.setAlignmentX(12);
         panel.add(contactLabel);
 
         panel.add(Box.createRigidArea(new Dimension(1, 5)));
-
-        panel.add(new JLabel("Enter Your Email Address"));
-        JTextField userEmail = new JTextField();
-        userEmail.setSize(190, 20);
-        userEmail.setMaximumSize(new Dimension(190, 20));
+        JLabel emailLabel = new JLabel("Enter Your Email Address");
+        emailLabel.setAlignmentX(10);
+        panel.add(emailLabel);
+        JTextField userEmail = new JTextField(27);
+        userEmail.setPreferredSize(new Dimension(190, 27));
+        userEmail.setMaximumSize(userEmail.getPreferredSize());
         panel.add(userEmail);
 
-        panel.add(new JLabel("Body"));
+        JLabel bodyLabel = new JLabel("Body");
+        bodyLabel.setAlignmentX(16);
+        panel.add(bodyLabel);
         JTextArea emailBody = new JTextArea(50,50);
         //JScrollPane sPane = new JScrollPane(emailBody);
         emailBody.setSize(300, 60);
@@ -98,17 +104,17 @@ public class ContactForm implements Form {
 
 
 
-    // For testing
-    // public static void main(String[] args) {
-    //     JFrame frame = new JFrame();
-    //     ContactForm form = new ContactForm();
-    //     frame.add(form.display());
-    //     frame.setPreferredSize(new Dimension(524,368));
+    //For testing
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        ContactForm form = new ContactForm();
+        frame.add(form.display());
+        frame.setPreferredSize(new Dimension(524,368));
         
-    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    //     frame.pack();
-    //     frame.setLocationRelativeTo(null);
-    //     frame.setVisible(true);
-    // }
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
 
