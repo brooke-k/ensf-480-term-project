5f61575af7783db99269832113d4b49aff144bd6
Brooke Kindleman <72521263+brooke-k@users.noreply.github.com>
2021-12-06 20:28:50 -0700

    Nick/driver (#8)
    * testing
    
    * bug: Need to correct LoginListener constructor
    - Will need to create a new package for GUI listeners
    
    * commit
    
    * feat: Login panel reads login information
    
    * feat: Added focus listeners for multiple forms:
    - Contact
    - Listener
    - Login
    - Preference
    Updated LoginListener to Listener
    
    Co-authored-by: Nick <nickknapton12@gmail.com>

---
+++ app/src/main/java/ensf480/group14/Driver.java
@@ -0,0 +1,122 @@
+package ensf480.group14;
+
+import java.awt.event.ActionListener;
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import ensf480.group14.eventListeners.Listener;
+import ensf480.group14.forms.*;
+
+import java.awt.event.ActionEvent;
+
+enum FormState {
+    LOGIN,
+    RENTER_SIGNUP,
+    CONTACT,
+    PREFERENCE,
+    LANDLORD_SIGNUP,
+    SEARCH,
+    PROPERTY_APPLICATION,
+    ERROR;
+}
+
+public class Driver {
+    static JFrame frame1;
+    static JPanel frame;
+
+    // static LoginListener loginListener;
+
+    public static void main(String[] args) {
+        CardLayout cardLayout = new CardLayout();
+        frame = new JPanel(cardLayout);
+        frame1 = new JFrame();
+
+        Listener listener = new Listener();
+
+        LoginForm loginForm = LoginForm.getOnlyInstance(listener);
+        RenterSignUpForm signUp = new RenterSignUpForm();
+        ContactForm contact = new ContactForm();
+        PreferenceForm preferenceForm = new PreferenceForm();
+        LandlordSignUpForm landlordSign = new LandlordSignUpForm();
+        Search searchForm = new Search();
+        PropertyApplication propertyApp = new PropertyApplication();
+
+        frame.add(loginForm.display(), "loginForm");
+        frame.add(signUp.display(), "renterSignUpForm");
+        frame.add(contact.display(), "contactForm");
+        frame.add(preferenceForm.display(), "preferencesForm");
+        frame.add(searchForm.display(), "searchForm");
+        frame.add(landlordSign.display(), "landlordSignUpForm");
+
+        frame1.add(frame);
+        frame1.pack();
+        frame1.setLocationRelativeTo(null);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame1.setVisible(true);
+
+        while (true) {
+            String page = listener.getPageToShow();
+            if (page.equals("SignInPage")) {
+                cardLayout.show(frame, "renterSignUpForm");
+                System.out.println(loginForm.getUsername() + ", " + loginForm.getPassword());
+            } else if (page.equals("LoginPage")) {
+                cardLayout.show(frame, "loginForm");
+            } else {
+                cardLayout.show(frame, "loginForm");
+            }
+        }
+
+        /*
+         * FormState currState = FormState.LOGIN;
+         * LoginListener loginListener = new LoginListener();
+         * frame = new JFrame();
+         * LoginForm form = LoginForm.getOnlyInstance(loginListener);
+         * // ^^ Does not work. Need another package to deal with this.
+         * frame.add(form.display());
+         * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         * frame.pack();
+         * frame.setLocationRelativeTo(null);
+         * frame.setVisible(true);
+         * // System.out.println("\u001B[1;1H\u001B[2J");
+         * int s;
+         * while (true) {
+         * FormState curr = FormState.LOGIN;
+         * s = loginListener.getStringEvent();
+         * // System.out.println(s);
+         * // if (s != null && s.equals("Login")) {
+         * // System.out.println("LOGIN");
+         * // curr = FormState.LOGIN;
+         *
+         * // } else if (s != null && s.equals("Or Sign Up")) {
+         * // System.out.println("SIGNUP");
+         * // curr = FormState.SIGNUP;
+         * // }
+         * // System.out.println("\u001B[1;1H\u001B[2K" + curr + ", " + currState);
+         * // System.out.println(currState);
+         * // System.out.println(curr);
+         * // System.out.print(" ");
+         * // if (currState != curr) {
+         * // // System.out.println("\u001B[1;2H\u001B[2K" + "Test");
+         * // currState = curr;
+         * // System.out.println("te");
+         *
+         * switch (s) {
+         * case 1:
+         * System.out.println("LOGIN");
+         * break;
+         * case 2:
+         * System.out.println("SIGNUP");
+         * break;
+         * default:
+         * System.out.println("No Match");
+         * }
+         *
+         * }
+         *
+         * }
+         * // return;
+         */
+    }
+}
--- app/src/main/java/ensf480/group14/forms/ContactForm.java
+++ app/src/main/java/ensf480/group14/forms/ContactForm.java
@@ -24,24 +24,24 @@ public class ContactForm implements Form {
     // private String userEmailAddress;
     // private String message;
 
-    public JPanel display(){
+    public JPanel display() {
         JPanel panel = new JPanel();
         Dimension expectDimension = new Dimension(300, 300);
 
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-       // panel.setLayout(new GridLayout(0,1));
-        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        // panel.setLayout(new GridLayout(0,1));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setPreferredSize(expectDimension);
         panel.setMaximumSize(expectDimension);
         panel.setMinimumSize(expectDimension);
 
         panel.setBackground(Color.GRAY);
-        
-        //panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // panel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JLabel contactLabel = new JLabel("Contact");
         contactLabel.setFont(new Font("Serif", Font.BOLD, 35));
-        contactLabel.setSize(40,40);
+        contactLabel.setSize(40, 40);
         contactLabel.setAlignmentX(12);
         panel.add(contactLabel);
 
@@ -57,23 +57,22 @@ public class ContactForm implements Form {
         JLabel bodyLabel = new JLabel("Body");
         bodyLabel.setAlignmentX(16);
         panel.add(bodyLabel);
-        JTextArea emailBody = new JTextArea(50,50);
-        //JScrollPane sPane = new JScrollPane(emailBody);
+        JTextArea emailBody = new JTextArea(50, 50);
+        // JScrollPane sPane = new JScrollPane(emailBody);
         emailBody.setSize(300, 60);
         emailBody.setMaximumSize(new Dimension(300, 60));
         emailBody.setColumns(300);
         emailBody.setLineWrap(true);
         panel.add(emailBody);
 
-
         panel.add(Box.createRigidArea(new Dimension(1, 5)));
 
         JButton sendButton = new JButton("Send");
-        sendButton.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent evt){
-                if(evt.getSource() == sendButton){
-                    parseEmail(emailBody.getText(),userEmail.getText());
-                    
+        sendButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                if (evt.getSource() == sendButton) {
+                    parseEmail(emailBody.getText(), userEmail.getText());
+
                 }
             }
         });
@@ -82,39 +81,21 @@ public class ContactForm implements Form {
         return panel;
     }
 
-
-    Email parseEmail(String body,String sendAddr){
-        Email mail = new Email(body,sendAddr);
+    Email parseEmail(String body, String sendAddr) {
+        Email mail = new Email(body, sendAddr);
         return mail;
     }
 
-   
-
-    
-
-   
-
-   
-
-   
-
-   
-
-   
-
-
-
-    //For testing
+    // For testing
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         ContactForm form = new ContactForm();
         frame.add(form.display());
-        frame.setPreferredSize(new Dimension(524,368));
-        
+        frame.setPreferredSize(new Dimension(524, 368));
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
-
