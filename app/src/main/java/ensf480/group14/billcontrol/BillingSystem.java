922f7a23119824cf2fc8574745383e7470756904
Abhay Khosla <57297897+Abhay2412@users.noreply.github.com>
2021-12-06 20:31:41 -0700

    Manager User Created (#9)
    * Finished the BillingController and BillingSystem classes
    
    * Almost finished the user Manager need to figure how to connect it with the datbase
    
    * Added that the manager can set the periodDuration to something which is predefined

--- app/src/main/java/ensf480/group14/billcontrol/BillingSystem.java
+++ app/src/main/java/ensf480/group14/billcontrol/BillingSystem.java
@@ -1,55 +1,44 @@
 package ensf480.group14.billcontrol;
-/*
- * import java.util.Date;
- * 
- * public class BillingSystem {
- * private double feeAmount;
- * private String periodDuration;
- * private Date paymentDate;
- * private String paymentType;
- * private Landlord landlord;
- * 
- * //Constructor
- * public BillingSystem(double feeAmount, Landlord landlord, Date paymentDate,
- * String paymentType)
- * {
- * this.feeAmount = feeAmount;
- * this.landlord = landlord;
- * this.paymentDate = paymentDate;
- * this.paymentType = paymentType;
- * }
- * //Start of getters and setters
- * public double getFeeAmount() {
- * return this.feeAmount;
- * }
- * 
- * public void setFeeAmount(double feeAmount) {
- * this.feeAmount = feeAmount;
- * }
- * 
- * public String getPeriodDuration() {
- * return this.periodDuration;
- * }
- * 
- * public void setPeriodDuration(String periodDuration) {
- * this.periodDuration = periodDuration;
- * }
- * 
- * public Date getPaymentDate() {
- * return this.paymentDate;
- * }
- * 
- * public void setPaymentDate(Date paymentDate) {
- * this.paymentDate = paymentDate;
- * }
- * 
- * public String getPaymentType() {
- * return this.paymentType;
- * }
- * 
- * public void setPaymentType(String paymentType) {
- * this.paymentType = paymentType;
- * }
- * 
- * }
- */
+
+import java.util.Date;
+
+public class BillingSystem {
+    private double feeAmount;
+    private Date paymentDate;
+    private String paymentType;
+    private Landlord landlord;
+
+    //Constructor 
+    public BillingSystem(double feeAmount, Landlord landlord, Date paymentDate, String paymentType)
+	{
+		this.feeAmount = feeAmount;
+		this.landlord = landlord;
+		this.paymentDate = paymentDate;
+        this.paymentType = paymentType;
+	}
+    //Start of getters and setters 
+    public double getFeeAmount() {
+        return this.feeAmount;
+    }
+
+    public void setFeeAmount(double feeAmount) {
+        this.feeAmount = feeAmount;
+    }
+
+    public Date getPaymentDate() {
+        return this.paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPaymentType() {
+        return this.paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+}
